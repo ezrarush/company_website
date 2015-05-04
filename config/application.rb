b9b01514a1dd1ab113eb583f3c21e12f460781a9
Ezra Rush <rushwest@gmail.com>
2015-05-03 22:13:44 -0500

    bootstrap icon now work

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module CompanyWebsite
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # for bootstrap icons
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
